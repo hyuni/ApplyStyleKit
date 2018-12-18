8a3de09181ffa757bfa239fb770662c01fafb700
shindyu <shindyu.dev@gmail.com>
2018-12-18 20:01:41 +0900

    Add Tests

--- ApplyStyleKit/UIKit+ApplyStyle/UIStackView+ApplyStyle.swift
+++ ApplyStyleKit/UIKit+ApplyStyle/UIStackView+ApplyStyle.swift
@@ -2,50 +2,50 @@
 //  UIStackView+ApplyStyle.swift
 //  ApplyStyleKit
 //
-//  Created by 新堂 敬隆 on 2018/12/18.
+//  Created by shindyu on 2018/12/18.
 //  Copyright © 2018年 shindyu. All rights reserved.
 //
 
 import UIKit
 
 extension StyleObject where Base: UIStackView {
-    @discardableResult func addArrangedSubview(_ view: UIView) -> StyleObject {
+    @discardableResult public func addArrangedSubview(_ view: UIView) -> StyleObject {
         base.addArrangedSubview(view)
         return self
     }
 
-    @discardableResult func removeArrangedSubview(_ view: UIView) -> StyleObject {
+    @discardableResult public func removeArrangedSubview(_ view: UIView) -> StyleObject {
         base.removeArrangedSubview(view)
         return self
     }
 
-    @discardableResult func insertArrangedSubview(_ view: UIView, at stackIndex: Int) -> StyleObject {
+    @discardableResult public func insertArrangedSubview(_ view: UIView, at stackIndex: Int) -> StyleObject {
         base.insertArrangedSubview(view, at: stackIndex)
         return self
     }
 
-    @discardableResult func axis(_ axis: NSLayoutConstraint.Axis) -> StyleObject {
+    @discardableResult public func axis(_ axis: NSLayoutConstraint.Axis) -> StyleObject {
         base.axis = axis
         return self
     }
 
-    @discardableResult func distribution(_ distribution: UIStackView.Distribution) -> StyleObject {
+    @discardableResult public func distribution(_ distribution: UIStackView.Distribution) -> StyleObject {
         base.distribution = distribution
         return self
     }
 
-    @discardableResult func alignment(_ alignment: UIStackView.Alignment) -> StyleObject {
+    @discardableResult public func alignment(_ alignment: UIStackView.Alignment) -> StyleObject {
         base.alignment = alignment
         return self
     }
 
-    @discardableResult func spacing(_ spacing: CGFloat) -> StyleObject {
+    @discardableResult public func spacing(_ spacing: CGFloat) -> StyleObject {
         base.spacing = spacing
         return self
     }
 
     @available(iOS 11.0, *)
-    @discardableResult func setCustomSpacing(_ spacing: CGFloat, after arrangedSubview: UIView) -> StyleObject {
+    @discardableResult public func customSpacing(_ spacing: CGFloat, after arrangedSubview: UIView) -> StyleObject {
         base.setCustomSpacing(spacing, after: arrangedSubview)
         return self
     }
@@ -54,12 +54,12 @@ extension StyleObject where Base: UIStackView {
 //
 //    }
 
-    @discardableResult func isBaselineRelativeArrangement(_ isBaselineRelativeArrangement: Bool) -> StyleObject {
+    @discardableResult public func isBaselineRelativeArrangement(_ isBaselineRelativeArrangement: Bool) -> StyleObject {
         base.isBaselineRelativeArrangement = isBaselineRelativeArrangement
         return self
     }
 
-    @discardableResult func isLayoutMarginsRelativeArrangement(_ isLayoutMarginsRelativeArrangement: Bool) -> StyleObject {
+    @discardableResult public func isLayoutMarginsRelativeArrangement(_ isLayoutMarginsRelativeArrangement: Bool) -> StyleObject {
         base.isLayoutMarginsRelativeArrangement = isLayoutMarginsRelativeArrangement
         return self
     }
--- ApplyStyleKitTests/UIKit+ApplyStyle/UIStackView+ApplyStyleTests.swift
+++ ApplyStyleKitTests/UIKit+ApplyStyle/UIStackView+ApplyStyleTests.swift
@@ -2,7 +2,7 @@
 //  UIStackView+ApplyStyleTests.swift
 //  ApplyStyleKitTests
 //
-//  Created by 新堂 敬隆 on 2018/12/18.
+//  Created by shindyu on 2018/12/18.
 //  Copyright © 2018年 shindyu. All rights reserved.
 //
 
@@ -20,7 +20,7 @@ class UIStackView_ApplyStyleTests: XCTestCase {
     func test_addArrangedSubview() {
         let view = UIView()
 
-        stackView.addArrangedSubview(view)
+        stackView.applyStyle.addArrangedSubview(view)
 
         XCTAssertEqual(stackView.arrangedSubviews.count, 1)
         XCTAssertEqual(stackView.arrangedSubviews.last, view)
@@ -28,17 +28,17 @@ class UIStackView_ApplyStyleTests: XCTestCase {
 
     func test_removeArrangedSubview() {
         let view = UIView()
-
         stackView.addArrangedSubview(view)
-        stackView.removeArrangedSubview(view)
 
-        XCTAssertEqual(stackView.arrangedSubviews.last, view)
+        stackView.applyStyle.removeArrangedSubview(view)
+
+        XCTAssertEqual(stackView.arrangedSubviews.last, nil)
     }
 
     func test_insertArrangedSubview() {
         let view = UIView()
 
-        stackView.insertArrangedSubview(view, at: 0)
+        stackView.applyStyle.insertArrangedSubview(view, at: 0)
 
         XCTAssertEqual(stackView.arrangedSubviews.first, view)
     }
@@ -46,45 +46,63 @@ class UIStackView_ApplyStyleTests: XCTestCase {
     func test_axis() {
         let axis: NSLayoutConstraint.Axis = .horizontal
 
-        stackView.axis(axis)
+        stackView.applyStyle.axis(axis)
 
         XCTAssertEqual(stackView.axis, axis)
     }
 
     func test_distribution() {
         let distribution: UIStackView.Distribution = .equalSpacing
-        stackView.distribution(distribution)
+        stackView.applyStyle.distribution(distribution)
         XCTAssertEqual(stackView.distribution, distribution)
     }
 
     func test_alignment() {
-        let alignment = true
-        stackView.alignment(alignment)
+        let alignment: UIStackView.Alignment = .center
+
+        stackView.applyStyle.alignment(alignment)
+
         XCTAssertEqual(stackView.alignment, alignment)
     }
 
     func test_spacing() {
-        let spacing = true
-        stackView.spacing(spacing)
+        let spacing: CGFloat = 0.5
+
+        stackView.applyStyle.spacing(spacing)
+
         XCTAssertEqual(stackView.spacing, spacing)
     }
 
     @available(iOS 11.0, *)
     func test_setCustomSpacing() {
-        let setCustomSpacing = true
-        stackView.setCustomSpacing(setCustomSpacing)
-        XCTAssertEqual(stackView.setCustomSpacing, setCustomSpacing)
+        let viewA = UIView()
+        let viewB = UIView()
+        let viewC = UIView()
+        stackView.addArrangedSubview(viewA)
+        stackView.addArrangedSubview(viewB)
+        stackView.addArrangedSubview(viewC)
+        stackView.applyStyle.spacing(10.0)
+
+        let setCustomSpacing: CGFloat = 20.0
+
+        stackView.applyStyle.customSpacing(setCustomSpacing, after: viewA)
+
+        XCTAssertEqual(stackView.customSpacing(after: viewA), setCustomSpacing)
     }
 
     func test_isBaselineRelativeArrangement() {
         let isBaselineRelativeArrangement = true
-        stackView.isBaselineRelativeArrangement(isBaselineRelativeArrangement)
+
+        stackView.applyStyle.isBaselineRelativeArrangement(isBaselineRelativeArrangement)
+
         XCTAssertEqual(stackView.isBaselineRelativeArrangement, isBaselineRelativeArrangement)
     }
 
     func test_isLayoutMarginsRelativeArrangement() {
         let isLayoutMarginsRelativeArrangement = true
-        stackView.isLayoutMarginsRelativeArrangement(isLayoutMarginsRelativeArrangement)
+
+        stackView.applyStyle.isLayoutMarginsRelativeArrangement(isLayoutMarginsRelativeArrangement)
+
         XCTAssertEqual(stackView.isLayoutMarginsRelativeArrangement, isLayoutMarginsRelativeArrangement)
     }
 }
